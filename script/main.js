7781eb59035165ae78396471a075f24ee6797c48
braianleonardocano@gmail.com <braianleonardocano@gmail.com>
2022-12-09 10:55:21 -0300

    add last form

--- script/main.js
+++ script/main.js
@@ -8,15 +8,3 @@ let _name = ["Braian Cano"];
 
 writeEffect(typed, words, 8);
 writeEffect(name, _name, 15, false);
-
-const continueHover = $("#continue");
-continueHover.addEventListener("mouseover", (e) => {
-  let time = 1000;
-  let words = e.target.childNodes;
-  words.forEach((word) =>
-    word.animate([{ color: "red" }, { color: "blue" }], {
-      duration: (time += 1000),
-      iterations: Infinity,
-    })
-  );
-});
